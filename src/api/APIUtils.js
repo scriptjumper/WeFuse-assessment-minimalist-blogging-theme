791768f593c5e8fc3a3b86ad1b6f11954ea6a7c5
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-14 14:44:26 +0200

    Using axios and created method to call the api

---
+++ src/api/APIUtils.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const apiUrl = process.env.REACT_APP_API_URL
+export default {
+  getHomepage: () => {
+    /* return axios.get(`${apiUrl}/homepage`).then((res) => {
+      return res.data
+    }) */
+  },
+  getBlogList: () => {
+    // return axios.get(`${apiUrl}/blog/list`).then((res) => {
+    //   return res.data
+    // })
+  },
+  getBlogArticle: (slug) => {
+    // return axios.get(`${apiUrl}/blog/${slug}`).then((res) => {
+    //   return res.data
+    // })
+  }
+}
