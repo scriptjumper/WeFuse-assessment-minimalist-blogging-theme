011cbb8df796aa9a62f9c8154260392c9e4a44f9
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-14 13:11:03 +0200

    Sorting files/folders

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App welcomes you to Minimalist Blogging Theme</h1>
+    </div>
+  )
+}
+
+export default App
