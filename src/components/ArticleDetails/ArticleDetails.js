a69159bcc9300e52903badc72b0dd1301defbc6e
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-15 13:18:37 +0200

    passing props and converting string into html to render on the view

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
-function ArticleDetails() {
+const ArticleDetails = (props) => {
   return (
     <div>
-      <h1>Article 1</h1>
-
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni aut maiores nesciunt, perferendis laboriosam, atque cupiditate placeat assumenda
-        suscipit temporibus aliquam illo laudantium, velit fugit accusamus nihil culpa quis nobis.
-      </p>
+      <p dangerouslySetInnerHTML={{ __html: props.article.pageData }} />
     </div>
   )
 }
