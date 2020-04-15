016a8a44cc0a8fe605f31116ca3972d48604ff61
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-15 11:40:08 +0200

    Passing props down to child component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,54 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 
 import Header from './Header/Header'
 import ArticleDetails from './ArticleDetails/ArticleDetails'
 import ArticleList from './ArticleList/ArticleList'
 
-function App() {
-  return (
-    <Router>
-      <Header></Header>
+class App extends React.Component {
+  constructor() {
+    super()
 
-      <Route exact path="/" component={ArticleList}></Route>
-      <Route exact path="/article" component={ArticleDetails}></Route>
-    </Router>
-  )
+    this.state = {
+      articles: []
+    }
+  }
+
+  componentDidMount() {
+    let articles = [
+      {
+        category: 'Tech',
+        title: 'article 1',
+        date: '2020/24/03',
+        author: 'admin'
+      },
+      {
+        category: 'Accounting',
+        title: 'article 2',
+        date: '2015/12/11',
+        author: 'unknown'
+      },
+      {
+        category: 'Farming',
+        title: 'article 3',
+        date: '2015/12/11',
+        author: 'unknown'
+      }
+    ]
+    this.setState({ articles })
+  }
+
+  render() {
+    return (
+      <Router>
+        <Header></Header>
+
+        <Route exact path="/" render={(props) => <ArticleList articles={this.state.articles} />} />
+        <Route exact path="/article" component={ArticleDetails}></Route>
+      </Router>
+    )
+  }
 }
 
 export default App
