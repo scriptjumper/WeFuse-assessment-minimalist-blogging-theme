ba14d7c45c5378581bf6a1ec13d35b2149ac0916
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-15 10:21:08 +0200

    uUing react router to load different components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
+
+import Header from './Header/Header'
+import ArticleDetails from './ArticleDetails/ArticleDetails'
+import ArticleList from './ArticleList/ArticleList'
 
 function App() {
   return (
-    <div className="App">
-      <h1>App welcomes you to Minimalist Blogging Theme</h1>
-    </div>
+    <Router>
+      <Header></Header>
+
+      <Route exact path="/" component={ArticleList}></Route>
+      <Route exact path="/article" component={ArticleDetails}></Route>
+    </Router>
   )
 }
 
