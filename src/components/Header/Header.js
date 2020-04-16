674636f8eee578ac613f2fec959f36d8bc0b0e73
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-16 09:57:04 +0200

    Styled header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,26 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Blogs</Link>
-        </li>
-      </ul>
-    </div>
+    <header className="header">
+      <div className="navbar">
+        <img src="./logo192.png" alt="logo" className="logo" height="30" />
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn">
+          <span className="navicon"></span>
+        </label>
+        <ul className="menu">
+          <li>
+            <Link to="/">Blogs</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="header_content">
+        <h1>This is a test Assessment</h1>
+        <hr />
+        <p>Please make sure that you implement everything</p>
+      </div>
+    </header>
   )
 }
 
