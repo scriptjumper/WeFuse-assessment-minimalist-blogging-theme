7a02a9fc0b109a080393603420c8c15fddde1f9b
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-17 08:19:38 +0200

    Calling Homepage endpoint and displaying data in the view

--- src/api/APIUtils.js
+++ src/api/APIUtils.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 const apiUrl = process.env.REACT_APP_API_URL
 export default {
   getHomepage: () => {
-    /* return axios.get(`${apiUrl}/homepage`).then((res) => {
+    return axios.get(`${apiUrl}/homepage`).then((res) => {
       return res.data
-    }) */
+    })
   },
   getBlogList: () => {
     return axios.get(`${apiUrl}/blog/list`).then((res) => {
