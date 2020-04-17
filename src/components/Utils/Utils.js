367ca592cfaf7eae6671497614a7a747dacb548a
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-17 09:07:07 +0200

    Correcting comment

--- src/components/Utils/Utils.js
+++ src/components/Utils/Utils.js
@@ -1,5 +1,5 @@
 export default {
-  // Format date to MM/dd/yyyy
+  // Format date to look like eg 'Mar 24, 2020'
   dateFormatter: (dateStr) => {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     let date = new Date(dateStr)
